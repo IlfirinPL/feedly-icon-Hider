77756c0daa67de8cc9cd60e094b23b4ec0fc414e
Marcin Koperski <marcin.koperski+github@gmail.com>
2021-08-23 01:38:06 +0200

    Create feedly-icon-hider.js

---
+++ feedly-icon-hider.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Feedly Hide Icon 
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Marcin Koperski
+// @match        https://feedly.com/*
+// @icon         https://www.google.com/s2/favicons?domain=feedly.com
+// @grant        none
+// ==/UserScript==
+
+var refreshRate = 1000
+setInterval(function () {
+
+
+    function hideIcon() {
+        $('[title="Create Team Newsletter"]').hide();
+        $('[title="Share with teammates"]').hide();
+        $('[aria-label="Power Search"]').hide();
+        $('[aria-label="New Features"]').hide();
+        $('[aria-label="Learn and Get Support"]').hide();
+    };
+
+    hideIcon()
+
+
+
+}, refreshRate);
